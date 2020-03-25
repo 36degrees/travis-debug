ecc902d8ce0549fcf518429a9e84be5b18af7244
Oliver Byford <oliver.byford@digital.cabinet-office.gov.uk>
2020-03-25 17:39:49 +0000

    Moar test

--- script.sh
+++ script.sh
@@ -5,6 +5,12 @@ things=(
   buzz
 )
 
+# Unless the APPLY environment variable is provided, just do a dry run and show
+# the changes that we would make.
+if [[ -z "$FOO" ]]; then
+  things+=(bang)
+fi
+
 for thing in ${things[*]}; do
   echo "$thing"
 done
